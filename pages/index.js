6bdd37698db9d7aeff478a85a9403a3eabc3bcef
Andrea Almiron <andreamontes.rei@gmail.com>
2023-06-21 14:10:12 -0600

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,31 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
 
 export default function Home() {
+  const siteTitle = "My Blog";
+  const selfIntroduction = `Hello everyone! My name is Andrea Montes, and I am a dedicated stay-at-home mother of two wonderful children, aged 1 and 6. Beyond my role as a mother, I have embarked on an exciting journey of personal growth and professional development. Currently, I am immersing myself in the world of coding and software development, pursuing my passion for technology.
+
+  To further enhance my skills and knowledge in this field, I am attending a full-stack developer bootcamp at MIT xPro, one of the world's renowned institutions for cutting-edge education. Through this intensive program, I am acquiring a deep understanding of various programming languages, frameworks, and tools, empowering me to create innovative and impactful solutions.
+
+  Balancing the responsibilities of motherhood with the demands of learning to code has been both challenging and rewarding. It has taught me invaluable lessons in time management, determination, and perseverance. By investing in my education and honing my coding skills, I aim to create new opportunities for myself and contribute to the ever-evolving tech industry.
+
+  I believe that being a mother has nurtured my ability to adapt, solve problems creatively, and work effectively within a team. These transferable skills, combined with my newfound technical expertise, make me a versatile and resilient professional ready to take on exciting challenges.
+
+  I am thrilled to connect with like-minded individuals, collaborate on inspiring projects, and make a meaningful impact in the world of technology. Together, let's push boundaries, drive innovation, and create a brighter future.`;
+
   return (
-    <div className={styles.container}>
+    <Layout home>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{siteTitle}</title>
       </Head>
-
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
+      <section className={utilStyles.headingMd}>
+        <p>{selfIntroduction}</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+      </section>
+    </Layout>
+  );
 }
